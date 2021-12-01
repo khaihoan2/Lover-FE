6c1d3c82f37b751d0f5f81c73b07e8c8d4f36763
ThanhTran <tranvanthanhtran.tt@gmail.com>
2021-12-01 13:13:09 +0700

    cắt template buyer thành các component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'buyer',
+    loadChildren: () => import('./buyer/buyer.module').then(module => module.BuyerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
